fb181c1cb72e471ddf320f5a7a1573aaea175b32
Alexandru Chirila <chirilaalexandru90@gmail.com>
2020-04-22 17:19:41 +0300

    Css dummy import

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const webpack = require('webpack'); //to access built-in plugins
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      { test: /\.txt$/,
+        use: 'raw-loader'
+      }
+    ],
+    // plugins: [
+    //   new HtmlWebpackPlugin({template: './src/index.html'})
+    // ]
+  }
+};
